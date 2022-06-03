555bf2d641824fa449b0e49e1387958860d593ff
daletools <dalerhendricks@outlook.com>
2022-06-03 14:06:40 -0700

    resubmit

--- traits.js
+++ traits.js
@@ -1,7 +1,7 @@
 const traits = {
     race: [['Dragonborn', 18, 80], ['Dwarf', 18, 350], ['Elf', 18, 750], ['Gnome', 18, 475], ['Half-Elf', 18, 180], ['Half-Orc', 18, 75], ['Halfling', 18, 150], ['Human', 18, 80], ['Tiefling', 18, 100]],
     hairColor: ['black, dark brown, light brown', 'chestnut brown', 'auburn', 'red', 'orange red', 'copper', 'strawberry blonde', 'light blonde', 'golden blonde', 'dark blonde', 'grey', 'white', 'none'],
-    profession: ['adventurer', 'farmer', 'cobbler', 'guard', 'soldier', 'acolyte', 'merchant'],
+    profession: ['adventurer', 'farmer', 'craftsman', 'guard', 'soldier', 'acolyte', 'merchant'],
     home: ['capital city', 'large city', 'medium city', 'small town', 'village'],
     temper: {
         C: 3,
