2369027edc83ad33cfea57a5433a692792bc086c
daletools <dalerhendricks@outlook.com>
2022-06-09 17:03:44 -0700

    added temperament data

--- main.js
+++ main.js
@@ -13,7 +13,8 @@ let inputs = {
     ideal: null,
     flaw: null,
     //array of unfilled values
-    blanks: [],             
+    blanks: [],          
+    canoe: [],   
 
     //read values from html form and 
     assign() {
@@ -33,44 +34,83 @@ let inputs = {
     },
 
     assess() {
-        let usedRace;   //stores index of chosen race for further calculations
+        this.setRace();
+        this.setAge();
+        this.setHome();
+        this.setProfession();
+        this.setTemper();
+        this.setAlignment();
+        output();
+    },
+
+    setRace() {
+            //creates key 'usedRace' to store index of chosen race for further calculations
         if (this.blanks.includes("race")) {         //
-            usedRace = Math.floor(Math.random() * (traits.race.length - 1));
-            this.race = traits.race[usedRace][0];
+            this.usedRace = Math.floor(Math.random() * (traits.race.length - 1));
+            this.race = traits.race[this.usedRace][0];
             console.log(`assigned race is ${this.race}`);
         } else if (traits.race.flat().includes(this.race)) {       //if user provides SRD race
-            usedRace = traits.race.flat().indexOf(this.race);
-            if (usedRace != 0) {
-                usedRace /= 3;
+            this.usedRace = traits.race.flat().indexOf(this.race);
+            if (this.usedRace != 0) {
+                this.usedRace /= 3;
             }
-            console.log(`user chose ${this.race} at index ${usedRace}`);
+            console.log(`user chose ${this.race} at index ${this.usedRace}`);
         } else {
-            usedRace = 7;       //if user provides custom race, use human data
+            this.usedRace = 7;       //if user provides custom race, use human data
             console.log(`user chose a custom race called ${this.race}`);
         }
+    },
 
+    setAge() {
         if (document.getElementById("randomizeAge")) {
             this.age = Math.floor(Math.random() * 100);
             document.getElementById("ageInput").value = this.age;
         }
-        this.age = Math.floor(this.age * ((traits.race[usedRace][2] - traits.race[usedRace][1]) / 100) + traits.race[usedRace][1]);     //assigns age based on race
+        this.age = Math.floor(this.age * ((traits.race[this.usedRace][2] - traits.race[this.usedRace][1]) / 100) + traits.race[this.usedRace][1]);     //assigns age based on race
         console.log(`assigned age is ${this.age}`);
+    },
 
+    setHome() {
         if (this.blanks.includes("home")) {
             this.home = traits.home[Math.floor(Math.random() * traits.home.length)];
         }
+    },
 
+    setProfession() {
         if (this.blanks.includes("profession")) {
             this.profession = traits.profession[Math.floor(Math.random() * traits.profession.length)];
         }
-        
-        output();
+    },
+
+    setTemper() {
+        this.canoe.length = 0;      //reset canoe array
+        this.orderedCanoe.length = 0;
+
+        for (let i = 0; i < 5; i++) {
+            this.canoe.push(Math.floor(Math.random() * 4));     //fill canoe array
+        }
+        console.log(`CANOE results are ${this.canoe}`);
+
+        let tempMax = this.canoe.indexOf(Math.max(...this.canoe));
+        const orderedCanoe = this.canoe.map();
+
+        for (i = 0; i < 4; i++) {
+
+            let diff;
+            this.temperament += traits.temper[this.canoe[i]][diff][canoe] + ", ";
+        }
+    },
+
+    setAlignment() {
+        this.alignment = traits.alignment[0][Math.floor(Math.random() * 3)] + " " + traits.alignment[1][Math.floor(Math.random() * 3)]
     }
+
 }
 
 function output() {
     console.log('I output the results!');
-    document.getElementById("textOutput").innerHTML = `${inputs.name} is a ${inputs.age} year old ${inputs.race} ${inputs.profession}.  They hail from a ${inputs.home}.`;
+    document.getElementById("textOutput").innerHTML = `${inputs.name} is a ${inputs.age} year old ${inputs.race} ${inputs.profession}.  
+                                                        They hail from a ${inputs.home}, and tend toward a ${inputs.alignment} alignment.`;
 }
 
 document.getElementById('generate').onclick = () => {
--- traits.js
+++ traits.js
@@ -1,16 +1,84 @@
 const traits = {
     race: [['Dragonborn', 18, 80], ['Dwarf', 18, 350], ['Elf', 18, 750], ['Gnome', 18, 475], ['Half-Elf', 18, 180], ['Half-Orc', 18, 75], ['Halfling', 18, 150], ['Human', 18, 80], ['Tiefling', 18, 100]],
+    
     hairColor: ['black, dark brown, light brown', 'chestnut brown', 'auburn', 'red', 'orange red', 'copper', 'strawberry blonde', 'light blonde', 'golden blonde', 'dark blonde', 'grey', 'white', 'none'],
+    
     profession: ['adventurer', 'farmer', 'craftsman', 'guard', 'soldier', 'acolyte', 'merchant'],
+   
     home: ['capital city', 'large city', 'medium city', 'small town', 'village'],
-    temper: {
-        C: 3,
-        A: 3,
-        N: 3,
-        O: 3,
-        E: 3
-    },
-    alignment: [["lawful", "neutral", "chaotic"], ["lawful", "neutral", "chaotic"], ["lawful", "neutral", "chaotic"]]   //good, neutral, evil
+
+    temper: [
+        [ //Positive/Positive intersection
+            [ //Conscientiousness
+                "organized", "helpful", "steady", "perceptive", "persistent"
+            ],
+            [ //Agreeableness
+                "dependable", "warm", "patient", "diplomatic", "sociable"
+            ],
+            [ //Neuroticism/stability
+                "thorough", "tolerant", "unenvious", "inventive", "bold"
+            ],
+            [ //Openness
+                "industrious", "tactful", "versatile", "creative", "dramatic"
+            ],
+            [ //Extraversion
+                "alert", "cheerful", "confident", "worldly", "talkative"
+            ]
+        ],
+        [ //Positive/negative intersection
+            [ //Conscientiousness
+                null, "lenient", "informal", null, "mischevious"
+            ],
+            [ //Agreeableness
+                "stern", null, "stoic", "individualistic", "forceful"
+            ],
+            [ //Neuroticism
+                null, "sensitive", null, "affectionate", "fliratious"
+            ],
+            [ //Openness
+                "traditional", null, "steadfast", null, "wordy"
+            ],
+            [ //Extraversion
+                "cautious", "agreeable", "unassuming", "introspective", null
+            ]
+        ],
+        [ //Negative/positive intersection
+            [ //Conscientiousness
+                null, "rigid", "particular", "obstinate", "reserved"
+            ],
+            [ //Agreeableness
+                null, null, "gullible", "dependent", "submissive"
+            ],
+            [ //Neuroticism
+                "complacent", "insensitive", null, "unsophisticated", "sleepy"
+            ],
+            [ //Openness
+                "unconventional", "shrewd", "paranoid", null, null
+            ],
+            [ //Extraversion
+                "reckless", "crude", "high-strung", "pompous", null
+            ]
+        ],
+        [ //Negative/Negative intersection
+            [ //Conscientiousness
+                "careless", "rude", "nosey", "ignorant", "sluggish"
+            ],
+            [ //Agreeableness
+                "unreliable", "unkind", "irritable", "shallow", "reclusive"
+            ],
+            [ //Neuroticism
+                "inconsistent", "selfish", "jealous", null, "cowardly"
+            ],
+            [ //Openness
+                "immature", "uncharitable", "contemptuous", "closed-minded", "meek"
+            ],
+            [ //Extraversion
+                "lazy", "cold", "insecure", "inarticulate", "shy"
+            ]
+        ]
+    ],
+
+    alignment: [['lawful', 'neutral', 'chaotic'],['good', 'neutral', 'evil']]
 };
 
 export { traits };
